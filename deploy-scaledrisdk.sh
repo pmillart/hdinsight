5995d8add5d545c534b24aed15d01e4fd3c389ae
Patrick Millart <pamillar@microsoft.com>
2018-09-17 22:22:35 +0200

    Add Tracking Deployment

--- deploy-scaledrisdk.sh
+++ deploy-scaledrisdk.sh
@@ -1,9 +1,8 @@
-set -x
-MYRESOURCEGROUP=ScaledRiskarm
+MYRESOURCEGROUP=ScaledRiskARM
 #Create Resource Group 
 az group create --name $MYRESOURCEGROUP --location "West Europe"
 
-az group deployment create \
+time az group deployment create \
   --name ScaledRiskDeployment \
   --resource-group $MYRESOURCEGROUP \
   --template-uri "https://raw.githubusercontent.com/pmillart/hdinsight/master/azuredeploy.json" \
