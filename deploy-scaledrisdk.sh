6173803484f74a06c42628502e7dffee5437c991
Patrick Millart <pamillar@microsoft.com>
2018-09-18 16:23:34 +0200

    Running cluster with front

--- deploy-scaledrisdk.sh
+++ deploy-scaledrisdk.sh
@@ -1,9 +1,11 @@
 MYRESOURCEGROUP=ScaledRiskARM
-#Create Resource Group 
+PARTNERID=pid-9aa77d5f-a637-4e77-a3ce-51e9f7048fbb
+time  az group delete --name $MYRESOURCEGROUP --yes
 az group create --name $MYRESOURCEGROUP --location "West Europe"
 
 time az group deployment create \
-  --name ScaledRiskDeployment \
+  --name $PARTNERID \
+  --mode Incremental \
   --resource-group $MYRESOURCEGROUP \
-  --template-uri "https://raw.githubusercontent.com/pmillart/hdinsight/master/azuredeploy.json" \
+  --template-file azuredeploy.json \
   --parameters @azuredeploy.parameters.json
