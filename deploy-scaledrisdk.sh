6faa3bbcae55295da7ee8f9ed83c18703c5d79d5
Patrick Millart <pamillar@microsoft.com>
2018-09-19 10:38:41 +0200

    other tests

--- deploy-scaledrisdk.sh
+++ deploy-scaledrisdk.sh
@@ -1,8 +1,10 @@
 MYRESOURCEGROUP=ScaledRiskARM
 PARTNERID=pid-9aa77d5f-a637-4e77-a3ce-51e9f7048fbb
+echo "Delete ResourceGroupe "|| $MYRESOURCEGROUP
 time  az group delete --name $MYRESOURCEGROUP --yes
+echo "Create ResourceGroupe "|| $MYRESOURCEGROUP
 az group create --name $MYRESOURCEGROUP --location "West Europe"
-
+echo "Deploy HDInsight"
 time az group deployment create \
   --name $PARTNERID \
   --mode Incremental \
